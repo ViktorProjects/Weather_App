63cd8d32b681db1436bad88cd5df59b6542c44bd
React <3809164@gmail.com>
2024-01-09 15:29:35 +0200

    create app

---
+++ weather_app/src/Components/Weather.jsx
@@ -0,0 +1,50 @@
+import React, {useState} from 'react';
+import axios from 'axios'
+
+const Weather = () => {
+    const [data, setData] = useState({})
+    const [location, setLocation] = useState('')
+
+    const searchLocation = (event) => {
+        if (event.key === 'Enter') {
+            axios.get(url).then((response) => {
+                setData(response.data)
+                console.log(response.data)
+            })
+            setLocation("")
+        }
+    }
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=27b03c36814d3922111246472acf151d`
+
+    return (
+        <div className={"max-w-[700px] m-auto p-4"}>
+            <div className={"border-2 my-4"}>
+                <input
+                    className={"w-full p-2"}
+                    value={location}
+                    onChange={event => setLocation(event.target.value)}
+                    placeholder={"Enter Location"}
+                    onKeyPress={searchLocation}
+                    type="text"/>
+            </div>
+            <div className={"w-full h-full flex flex-col text-center justify-center items-center"}>
+                <div>{data.name}</div>
+                <div>
+                    {data.main ? <h1>{data.main.temp.toFixed()}°C</h1> : null}
+                </div>
+                <div>
+                    {data.weather ? <p>{data.weather[0].main}</p> : null}
+                </div>
+                <div>
+                    {data.main ? <p className='bold'>{data.main.humidity}%</p> : null}
+                </div>
+                <div>
+                    {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH</p> : null}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Weather;
